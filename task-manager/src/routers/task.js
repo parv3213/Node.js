8dd71b49c5a52f6bf557d3f129a8f3c0dbe96040
parv3213 <parv3213@gmail.com>
2020-07-02 08:20:39 +0530

    paging, sorting, match

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,9 +18,33 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+	const match = {};
+	const sort = {};
+
+	if (req.query.completed) {
+		match.completed = req.query.completed === "true";
+	}
+
+	if (req.query.sortBy) {
+		const parts = req.query.sortBy.split(":");
+		sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+	}
+
 	try {
-		const tasks = await Task.find({ owner: req.user._id });
-		res.send(tasks);
+		// const tasks = await Task.find({ owner: req.user._id });
+		await req.user
+			.populate({
+				path: "tasks",
+				match,
+				// pagination
+				options: {
+					limit: parseInt(req.query.limit),
+					skip: parseInt(req.query.skip),
+					sort,
+				},
+			})
+			.execPopulate();
+		res.send(req.user.tasks);
 	} catch (e) {
 		res.status(500).send();
 	}
